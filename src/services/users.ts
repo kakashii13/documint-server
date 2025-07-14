3b1e212f08155918d09762c26d10bc5e9b0c4cc4
kakashi <petunilo96@gmail.com>
2025-07-14 20:24:34 -0300

    fix prisma import

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,4 +1,4 @@
-import { Prisma } from "../generated/prisma";
+import { Prisma } from "@prisma/client";
 import { prisma } from "../prismaClient";
 import { User } from "../types/types";
 import { generateLink } from "../utils/generateLink";
